c85d46963f2e0cacd888b8ccb7103d979b0bacde
Fatima Z <67465113+fatimah-z@users.noreply.github.com>
2022-10-08 16:57:28 +0500

    Update firebase.js

--- frontend/eeg-app/firebase.js
+++ frontend/eeg-app/firebase.js
@@ -1,23 +1,27 @@
-// Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
+// import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIOwGpVUeLFzK-jgokGSknLkghMLBIyI8",
-  authDomain: "eeg-app-31823.firebaseapp.com",
-  projectId: "eeg-app-31823",
-  storageBucket: "eeg-app-31823.appspot.com",
-  messagingSenderId: "92837153975",
-  appId: "1:92837153975:web:9b0ec9b3917aad18e39132"
+  apiKey: "AIzaSyBc2XLZOTvrjzv4Iw_Kge5rXhB9Ahgc-90",
+  authDomain: "test-3fc13.firebaseapp.com",
+  projectId: "test-3fc13",
+  storageBucket: "test-3fc13.appspot.com",
+  messagingSenderId: "981887438504",
+  appId: "1:981887438504:web:c65f0b171a1724e7ba3c9d",
+  measurementId: "G-QN2386FQG1",
 };
 
-// Initialize Firebase
-let app;
-if (firebase.getApps().length===0){
-    app = firebase.initializeApp(firebaseConfig);
-
-}else{
-    app= firebase.getApp()
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
+// Initialize Firebase
+export { firebase };
